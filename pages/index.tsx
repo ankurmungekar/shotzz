3bba38ee847471a553f087d7fb01b61f83bbd0cc
Ankur Mungekar <ankur.mungekar@newfold.com>
2022-09-27 16:32:13 +0530

    added navbar, sidebar and post api route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,27 @@
 import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
+import axios from 'axios'
+import { Video } from '../types';
 
-const Home: NextPage = () => {
+interface IProps {
+  videos: Video[];
+}
+
+const Home = ({ videos }: IProps) => {
+  console.log(videos);
   return (
-    <div className={styles.container}>
+    <div>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
     </div>
   )
 }
-
+export const getServerSideProps = async () => {
+  const response = await axios.get(`http://localhost:3000/api/post`);
+  return {
+    props: {
+      videos: response.data
+    }
+  }
+}
 export default Home
