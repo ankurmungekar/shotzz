780844e7884dec0a5bbf6b8d14a191cec582cdec
Ankur Mungekar <ankur.mungekar@newfold.com>
2022-09-29 12:17:34 +0530

    added google auth and store

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import Logo from '../../utils/logo.png'
+import { GoogleLogin } from '@react-oauth/google';
+import useAuthStore from '../../store/authStore';
+import { createOrGetUser } from '../../utils';
+import { IUser } from '../../types';
 
 const Navbar = () => {
+  const [user, setUser] = useState<IUser | null>();
+  const { userProfile, addUser } = useAuthStore();
+  useEffect(() => {
+    setUser(userProfile);
+  }, [userProfile]);
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
       <Link href='/'>
@@ -16,6 +25,33 @@ const Navbar = () => {
           />
         </div>
       </Link>
+      <div>Search</div>
+      <div>
+        {user ? (
+          <div className='flex gap-5 md:gap-10'>
+            {user.image && (
+              <Link href={`/profile/${user._id}`}>
+                <div>
+                  <Image
+                    className='rounded-full cursor-pointer'
+                    src={user.image}
+                    alt='user'
+                    width={40}
+                    height={40}
+                  />
+                </div>
+              </Link>
+            )}
+          </div>
+        ) : (<GoogleLogin
+          onSuccess={response => {
+            createOrGetUser(response, addUser);
+          }}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+        />)}
+      </div>
     </div>
   )
 }
